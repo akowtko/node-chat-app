e6095e4f0b59c589e5d2ac00e670f78d0540eab8
Nicole Kowtko <akowtko@hmc.edu>
2019-01-28 16:17:34 -0800

    emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server.');
-
-    socket.emit('createMessage', {
-       from: 'derp',
-       text: 'i do not want to'
-    });
 });
 socket.on('disconnect', function () {
     console.log('Disconnected from server.');
