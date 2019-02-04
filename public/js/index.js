e03e4ca7315d208cfae8f8518a7f32ff3633fdaa
Nicole Kowtko <akowtko@hmc.edu>
2019-02-03 18:45:08 -0800

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -23,23 +23,29 @@ socket.on('newLocationMessage', function(message) {
 });
 
 jQuery('#message-form').on('submit', function (e) {
+    const messageTextbox = jQuery('[name=message]');
     e.preventDefault();
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
+        messageTextbox.val('');
     })
 });
 
 const locationButton = jQuery('#send-location');
 locationButton.on('click', function () {
     if (!navigator.geolocation) return alert('Geolocation not supported by your browser.');
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
     navigator.geolocation.watchPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
