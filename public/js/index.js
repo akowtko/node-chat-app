3e02e7f2734bf253567371103413d08d7b584a92
Nicole Kowtko <akowtko@hmc.edu>
2019-02-05 15:36:27 -0800

    scroll to bottom if user close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+function scrollToBottom () {
+    // Selectors
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 socket.on('connect', function () {
     console.log('Connected to server.');
 });
@@ -15,6 +32,7 @@ socket.on('newMessage', function(message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html)
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -26,6 +44,7 @@ socket.on('newLocationMessage', function(message) {
         url: message.locationUrl,
     });
     jQuery('#messages').append(html)
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
